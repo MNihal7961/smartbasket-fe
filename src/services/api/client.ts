924821284a6b1da08aea33901f2cc2fa051ca9e4
MNihal7961 <mnihal7961@gmail.com>
2025-07-15 13:31:31 +0530

    axios interceptors and api client added

---
+++ src/services/api/client.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add a request interceptor to inject the token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Add a response interceptor to handle 401 errors
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('token');
+      // Dispatch a custom event to trigger Redux logout
+      window.dispatchEvent(new Event('app-logout'));
+      window.location.href = '/signin';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api; 
